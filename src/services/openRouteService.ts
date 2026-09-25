34470295b0b813622c51dccc5a2037a897418682
dario36ds <dario36ds@gmail.com>
2026-09-25 09:50:36 +0200

    Aggiornato url ORS

--- src/services/openRouteService.ts
+++ src/services/openRouteService.ts
@@ -2,26 +2,26 @@ import Config from 'react-native-config';
 
 type Coordinate = [number, number];
 
+const DIRECTIONS_URL =
+  'https://api.openrouteservice.org/v2/directions/cycling-road/geojson';
+
 export async function calculateRoute(coordinates: Coordinate[]) {
   if (!Config.ORS_API_KEY) {
     throw new Error('ORS_API_KEY non configurata');
   }
 
-  const response = await fetch(
-    'https://api.heigit.org/openrouteservice/v2/directions/cycling-road/geojson',
-    {
-      method: 'POST',
-      headers: {
-        Authorization: Config.ORS_API_KEY,
-        'Content-Type': 'application/json',
-        Accept: 'application/geo+json',
-      },
-      body: JSON.stringify({
-        coordinates,
-        elevation: true,
-      }),
+  const response = await fetch(DIRECTIONS_URL, {
+    method: 'POST',
+    headers: {
+      Authorization: Config.ORS_API_KEY,
+      'Content-Type': 'application/json',
+      Accept: 'application/geo+json',
     },
-  );
+    body: JSON.stringify({
+      coordinates,
+      elevation: true,
+    }),
+  });
 
   const responseText = await response.text();
 
